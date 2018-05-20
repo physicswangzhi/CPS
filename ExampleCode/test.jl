b4652837e5165157532192e21fdbb9b0c42afb01
physicswangzhi <physicswangzhi@gmail.com>
2018-05-20 17:58:27 -0500

    Update Slides

--- ExampleCode/test.jl
+++
@@ -1,6 +0,0 @@
-## Script which prints out the hostnames of the worker processes
-# Used in conjunction with a job script to ensure multi-node parallelism
-
-hosts = @parallel for i=1:120
-       run(`hostname`) end
-println(hosts)
